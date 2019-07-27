f500b3bbf966d25cf4e86183fa8d59bfbcd9741f
shahuji <sonushahuji4@gmail.com>
2019-07-27 10:02:37 +0530

    heat map highchart almost complete

--- routes/api/profile_api.js
+++ routes/api/profile_api.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
-// const db = require('../../config/db');
-// const Posts = require('../../model/postbody_model');
-// const User = require('../../model/user_model');
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op
 const {Users,Posts,Likes,Comments} = require('../../model/model_index');
 
 const multer = require('multer');
@@ -165,6 +164,24 @@ router.get('/profile/heap_map_data', function(req,res)
     })
     //var data = [[11, 4, 91],[9, 3, 48],[9, 2, 31],[8, 3, 64],[8, 2, 123],[8, 1, 97],[8, 0, 85],[7, 3, 32],[7, 2, 82],[7, 1, 1],[7, 0, 31],[6, 4, 96],[6, 3, 98],[6, 2, 88],[6, 1, 44],[0, 0, 1],[2, 1, 15],[2, 2, 123],[2, 3, 64],[2, 4, 52], [3, 0, 72],[3, 1, 132],[3, 2, 114],[3, 3, 19],[4, 1, 5],[4, 2, 8],[4, 3, 117],[4, 4, 115],[5, 0, 88],[5, 1, 32],[5, 2, 12],[5, 3, 6]];
     //res.send(data);
+})
+
+router.post('/profile/heap_map_event_details', function(req,res)
+{
+    const year = Number(req.body.year);
+    const month = Number(req.body.month);
+    Posts.findAll({where:{event_start_date:{[Op.like]:  '%' + '2019-07-29' + '%'}}})
+    .then((data)=>
+    {
+        console.log(data);
+        res.send(data);
+    })
+    .catch((err)=>
+    {
+        res.send(err);
+    })
+    
+    //res.send("hhhhhh");
 })
 
 
